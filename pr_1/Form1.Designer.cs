58990c2c761856439fcf57836b160a116108c6f3
Seniy_Jalilow Seniy_Jalilow <arseniy-3108@mail.ru>
2023-12-24 22:52:20 +0500

    Добавьте файлы проекта.

---
+++ pr_1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace pr_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnScore = new System.Windows.Forms.Button();
+            this.txtStr = new System.Windows.Forms.TextBox();
+            this.txtKolvo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSymb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите строку: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Количество символов: ";
+            // 
+            // btnScore
+            // 
+            this.btnScore.Location = new System.Drawing.Point(219, 151);
+            this.btnScore.Name = "btnScore";
+            this.btnScore.Size = new System.Drawing.Size(74, 29);
+            this.btnScore.TabIndex = 1;
+            this.btnScore.Text = "Подсчитать";
+            this.btnScore.UseVisualStyleBackColor = true;
+            this.btnScore.Click += new System.EventHandler(this.btnScore_Click);
+            // 
+            // txtStr
+            // 
+            this.txtStr.Location = new System.Drawing.Point(155, 33);
+            this.txtStr.Name = "txtStr";
+            this.txtStr.Size = new System.Drawing.Size(138, 20);
+            this.txtStr.TabIndex = 2;
+            // 
+            // txtKolvo
+            // 
+            this.txtKolvo.Location = new System.Drawing.Point(182, 112);
+            this.txtKolvo.Name = "txtKolvo";
+            this.txtKolvo.Size = new System.Drawing.Size(111, 20);
+            this.txtKolvo.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Введите символ \"ма\" или \"а\"";
+            // 
+            // txtSymb
+            // 
+            this.txtSymb.Location = new System.Drawing.Point(206, 74);
+            this.txtSymb.Name = "txtSymb";
+            this.txtSymb.Size = new System.Drawing.Size(87, 20);
+            this.txtSymb.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 215);
+            this.Controls.Add(this.txtSymb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtKolvo);
+            this.Controls.Add(this.txtStr);
+            this.Controls.Add(this.btnScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Строковые функции";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnScore;
+        private System.Windows.Forms.TextBox txtStr;
+        private System.Windows.Forms.TextBox txtKolvo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSymb;
+    }
+}
+
---
+++ pr_1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int NumberSymbols(string stroka, char symbol)
+        {
+            // Инициализация переменной для подсчета количества символов в строке.
+            int k = 0;
+            // Цикл для прохода по каждому символу в строке.
+            for (int i = 0; i < stroka.Length; i++)
+            {
+                // Если символы совпадают, увеличиваем счетчик
+                if (stroka[i] == symbol)
+                {
+                    // Если символы совпадают, увеличиваем счетчик
+                    k += 1;
+                }
+            }
+            // Возврат общего количества найденных символов в строке
+            return k;
+        }
+        private int NumberMA(string stroka)
+        {
+            // Инициализация переменной для подсчета встреч символов "ма" в тексте
+            int ma = 0;
+            // Цикл для прохода по каждому символу текста, за исключением последнего.
+            for (int i = 0; i < txtStr.Text.Length - 1; i++)
+            {
+                // Проверка, совпадает ли текущий и следующий символы с "м" и "а" соответственно.
+                if (txtStr.Text[i] == 'м' && txtStr.Text[i + 1] == 'а')
+                {
+                    // Увеличение счетчика в случае обнаружения сочетания "ма".
+                    ma++;
+                }
+            }
+            // Возврат общего количества встреч символов "ма" в тексте.
+            return ma;
+        }
+
+        private void btnScore_Click(object sender, EventArgs e)
+        {
+            // Сохранение введенного пользователем текста в переменную inputText.
+            string inputText = txtSymb.Text;
+            // Проверка, равен ли введенный текст "а".
+            if (inputText == "а")
+            {
+                // Вывод количества символов 'а' в текстовое поле txtKolvo.
+                txtKolvo.Text = NumberSymbols(txtStr.Text, 'а').ToString();
+            }
+            // Проверка, равен ли введенный текст "ма".
+            else if (inputText == "ма")
+            {
+                // Вывод количества вхождений подстроки 'ма' в текстовое поле txtKolvo.
+                txtKolvo.Text = NumberMA(txtStr.Text).ToString();
+            }
+        }
+    }
+}
